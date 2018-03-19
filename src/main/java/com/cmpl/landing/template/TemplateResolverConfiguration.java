2cb3190bc984b0d3a72a39cbcece195015d34530
lperrod <lperrod@cardiweb.com>
2018-03-19 13:28:33 +0100

    spring boot 2 + launching as jar

--- src/main/java/com/cmpl/landing/template/TemplateResolverConfiguration.java
+++ src/main/java/com/cmpl/landing/template/TemplateResolverConfiguration.java
@@ -1,14 +1,12 @@
 package com.cmpl.landing.template;
 
+import com.cmpl.landing.context.ContextHolder;
 import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
-
-import com.cmpl.landing.context.ContextHolder;
 
 @Configuration
 @PropertySource("classpath:/templates.properties")
@@ -29,14 +27,16 @@ public class TemplateResolverConfiguration {
     pagesResolver.setTemplateMode("HTML5");
     pagesResolver.setOrder(order);
     pagesResolver.setCacheable(false);
+    pagesResolver.setCheckExistence(true);
     templateEngine.addTemplateResolver(pagesResolver);
 
     FileTemplateResolver mobilePagesResolver = new FileTemplateResolver();
     mobilePagesResolver.setPrefix(contextHolder.getAcceleratedMobilePageTemplatePath());
     mobilePagesResolver.setSuffix(".html");
-    mobilePagesResolver.setTemplateMode("HTML5");
+    mobilePagesResolver.setTemplateMode("Legacy HTML5");
     mobilePagesResolver.setOrder(order + 1);
     mobilePagesResolver.setCacheable(false);
+    mobilePagesResolver.setCheckExistence(true);
     templateEngine.addTemplateResolver(mobilePagesResolver);
   }
 
